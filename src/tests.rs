308a33a7df4f81423111ea179ed0f9e5c44d072e
mate.kadlicsko <kadlicsko.mate@gmail.com>
2023-02-17 11:15:18 +0100

    minor changes

--- src/tests.rs
+++ src/tests.rs
@@ -1,21 +1,37 @@
 pub use crate::utils::*;
 pub use crate::polynomial;
+use num::BigInt;
 
 
 #[cfg(test)]
 mod tests {
+    use num::ToPrimitive;
+
     use super::*;
 
     #[test]
     fn test_gcd() {
-        assert_eq!(gcd(82289148, 82289148), 82289148);
-        assert_eq!(bgcd(82289148, 82289148), 82289148);
-        assert_eq!(bgcd(0, 82289148), 82289148);
-        assert_eq!(gcd(0, 82289148), 82289148);
-        assert_eq!(gcd(61345370799, 25515131265), 9);
-        assert_eq!(bgcd(61345370799, 25515131265), 9);
-        assert_eq!(gcd(45068332734, 75461940078), bgcd(45068332734, 75461940078));
-        assert_eq!(gcd(1125899906842624, 1024), bgcd(1125899906842624, 1024));
+        let a: u64 = 82289148;
+        let b: u64 = 61345370799;
+        let c: u64 = 25515131265;
+        let n1 = match "12012156372189739281216757210576102012012223892738921798326187361278368721631225467389874637823746372838746543782374637283476543728374637283746543728253243534255432354234523452345234523452345234523452345234523452345234623623523452345234582848248248284"
+                               .parse::<BigInt>() {
+                                    Ok(n) => n,
+                                    Err(_) => panic!("what")
+                                };
+        let n2 = match "123565214386213485217365412738765123873265321983721987398217398217893721983773265812"
+                                .parse::<BigInt>() {
+                                    Ok(n) => n,
+                                    Err(_) => panic!("what")
+                                };
+
+        assert_eq!(gcd(a, a), a);
+        assert_eq!(bgcd(a, a), a);
+        assert_eq!(bgcd(0 as u64, a), a);
+        assert_eq!(gcd(0 as u64, a), a);
+        assert_eq!(gcd(b, c), 9 as u64);
+        assert_eq!(bgcd(b, c), 9 as u64);
+        assert_eq!(gcd::<BigInt>(n1, n2).to_i8().expect("not a small enough number"), 76);
     }
 
     #[test]
@@ -23,14 +39,12 @@ mod tests {
         assert_eq!(jacobi(610, 987), -1);
         assert_eq!(jacobi(1001, 9907), -1);
         assert_eq!(jacobi(219, 383), 1);
-        assert_eq!(jacobi(123, 1234), 1);
-        assert_eq!(jacobi(2, 1234), 0);
         assert_eq!(jacobi(3, 27), 0);
     }
 
     #[test]
     fn test_is_perfect_power() {
-        assert_eq!(is_perfect_power(2*2*2*2*2), true);
+        assert_eq!(is_perfect_kth_power(2*2*2*2*2, 5), true);
         assert_eq!(is_perfect_power(2*2*2*2*2*3), false);
     }
 
