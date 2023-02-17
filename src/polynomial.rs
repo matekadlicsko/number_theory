faa2ab536e368e9b002a145256eb5304f96b0772
mate.kadlicsko <kadlicsko.mate@gmail.com>
2023-02-17 11:55:32 +0100

    minor fixes in polynomials

--- src/polynomial.rs
+++ src/polynomial.rs
@@ -1,5 +1,5 @@
 use std::cmp::{max, PartialEq};
-use std::ops::{Add, Mul, Sub};
+use std::ops::{Add, Mul, Sub, AddAssign};
 use num::traits::{Zero};
 
 
@@ -10,13 +10,13 @@ pub struct Polynomial<T> {
 
 impl<T> Polynomial<T> where
     T: Add<Output = T> + Mul<Output = T> + Sub<Output = T> +
-       Clone + Copy + PartialEq + Zero {
+       Clone + PartialEq + Zero {
     pub fn drop_trailing_zeros(&self) -> Self {
         if self.coeffs.len() <= 1 {
             return Self{coeffs: self.coeffs.to_vec()};
         }
-        let last_coeff = self.coeffs[self.coeffs.len() - 1];
-        if last_coeff == last_coeff + last_coeff {
+        let last_coeff = self.coeffs[self.coeffs.len() - 1].clone();
+        if last_coeff.is_zero() {
             return Self{coeffs: self.coeffs[..self.coeffs.len() - 1].to_vec()}.drop_trailing_zeros();
         }
         return Self{coeffs: self.coeffs.to_vec()};
@@ -28,7 +28,7 @@ impl<T> Polynomial<T> where
 
     pub fn nth_coeff(&self, &n: &usize) -> T {
         if n <= self.degree() {
-            return self.coeffs[n];
+            return self.coeffs[n].clone();
         }
         return T::zero();
     }
@@ -36,12 +36,12 @@ impl<T> Polynomial<T> where
 
 impl<T> Add<&Polynomial<T>> for &Polynomial<T> where
     T: Add<Output = T> + Mul<Output = T> + Sub<Output = T> +
-       Clone + Copy + PartialEq + Zero {
+       Clone + PartialEq + Zero {
     type Output = Polynomial<T>;
     fn add(self, other: &Polynomial<T>) -> Polynomial<T> {
         let mut coeffs: Vec<T> = Vec::new(); 
         for i in 0..=max(self.degree(), other.degree()) {
-            let ith_comp: T = self.nth_coeff(&i) + other.nth_coeff(&i);
+            let ith_comp: T = self.nth_coeff(&i).clone() + other.nth_coeff(&i).clone();
             coeffs.push(ith_comp);
         }
         return Polynomial{ coeffs };        
@@ -50,7 +50,7 @@ impl<T> Add<&Polynomial<T>> for &Polynomial<T> where
 
 impl<T> Mul<&Polynomial<T>> for &Polynomial<T> where
     T: Add<Output = T> + Mul<Output = T> + Sub<Output = T> +
-       Clone + Copy + PartialEq + Zero {
+       Clone + PartialEq + Zero + AddAssign {
     type Output = Polynomial<T>;
     fn mul(self, other: &Polynomial<T>) -> Polynomial<T> {
         let degree: usize = self.degree() + other.degree();
@@ -58,7 +58,7 @@ impl<T> Mul<&Polynomial<T>> for &Polynomial<T> where
 
         for i in 0..=self.degree() {
             for j in 0..=other.degree() {
-                coeffs[i + j] = coeffs[i + j] + self.coeffs[i] * other.coeffs[j];
+                coeffs[i + j] += self.coeffs[i].clone() * other.coeffs[j].clone();
             }
         }
         return Polynomial{ coeffs };        
@@ -67,7 +67,7 @@ impl<T> Mul<&Polynomial<T>> for &Polynomial<T> where
 
 impl<T> PartialEq<Polynomial<T>> for Polynomial<T> where
     T:  Add<Output = T> + Mul<Output = T> + Sub<Output = T> +
-        Clone + Copy + PartialEq + Zero {
+        Clone + PartialEq + Zero {
     fn eq(&self, other: &Polynomial<T>) -> bool {
         self.drop_trailing_zeros().coeffs == other.drop_trailing_zeros().coeffs
     }
