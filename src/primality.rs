29cacbbf705b84297ac2bd94f0f11c245302d451
mate.kadlicsko <kadlicsko.mate@gmail.com>
2023-02-18 22:43:24 +0100

    added Primality and PerfectPower enums

--- src/primality.rs
+++ src/primality.rs
@@ -1,25 +1,32 @@
 pub use crate::utils::{mod_exp, jacobi, is_perfect_power};
 pub use rand::{seq::IteratorRandom, thread_rng};
+pub use num::traits::Pow;
+
+pub enum Primality {
+    Prime,
+    Composite,
+    ProbablyPrime
+}
 
 pub mod probabilistic { 
     use super::*;
 
-    pub fn fermat_primality_test(n: u64, n_iter: usize) -> bool {
+    pub fn fermat_primality_test(n: u64, n_iter: usize) -> Primality {
         let mut rng = thread_rng();
         let v: Vec<u64> = (2..n).collect();
         let bases = v.iter().choose_multiple(&mut rng, n_iter);
         
         for a in bases {
             if mod_exp(*a, n - 1,  n) != 1 {
-                return false;
+                return Primality::Composite;
             }
         }
-        return true;
+        Primality::ProbablyPrime
     }
 
-    pub fn solovay_strassen_primality_test(n: u64, n_iter: usize) -> bool {
+    pub fn solovay_strassen_primality_test(n: u64, n_iter: usize) -> Primality {
         if n % 2 == 0 {
-            return false;
+            return Primality::Composite;
         }
         let mut rng = thread_rng();
         let v: Vec<u64> = (2..n).collect();
@@ -34,23 +41,24 @@ pub mod probabilistic {
     
             if jacobi_symbol == 0 {
                 println!("jacobi = 0");
-                return false;
+                return Primality::Composite;
             } else if mod_exp(*a, (n - 1) / 2, n) != jacobi_symbol {
                 println!("jacobi = {}", jacobi_symbol);
-                return false;
+                return Primality::Composite;
             }
         }
-        return true;
+        Primality::ProbablyPrime
     }
 
-    pub fn miller_rabin_primality_test(n: u64, n_iter: usize) -> bool {
+
+    pub fn miller_rabin_primality_test(n: u64, n_iter: u64) -> Primality {
         if n % 2 == 0 {
-            return false;
+            return Primality::Composite;
         }
     
         let mut rng = thread_rng();
         let v: Vec<u64> = (2..n).collect();
-        let bases = v.iter().choose_multiple(&mut rng, n_iter);
+        let bases = v.iter().choose_multiple(&mut rng, n_iter as usize);
     
         // n - 1 = 2^s * n', s > 1
         let s: u64 = (n - 1).trailing_zeros() as u64;
@@ -64,26 +72,21 @@ pub mod probabilistic {
             while (j <= s - 2) && (b != n - 1) {
                 b = b * b % n;
                 if b == 1 {
-                    return false;
+                    return Primality::Composite;
                 }
                 j +=1;
             }
             if b != n - 1 {
-                return false;
+                return Primality::Composite;
             }
         }
-        return true;
-    }
+        if n_iter < n {
+            return Primality::ProbablyPrime;
+        }
+        Primality::Prime
+        }
 }
 
 pub mod deterministic {
-    use super::is_perfect_power;
-
-    pub fn aks(n: u64) -> bool {
-        //TODO
-        if is_perfect_power(n) {
-            return false;
-        }
-        return true;
-    }
+    // TODO
 }
